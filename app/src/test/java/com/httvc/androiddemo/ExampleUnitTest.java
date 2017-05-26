1d946cb2c39c3eb13fbe7ac61e7db1001055bae0
贾富杰 <httvc>
2017-05-27 00:09:26 +0800

    ssss

--- app/src/test/java/com/httvc/androiddemo/ExampleUnitTest.java
+++ app/src/test/java/com/httvc/androiddemo/ExampleUnitTest.java
@@ -1,6 +1,11 @@
 package com.httvc.androiddemo;
 
+import android.util.Log;
+
 import org.junit.Test;
+
+import rx.Observable;
+import rx.functions.Action1;
 
 import static org.junit.Assert.*;
 
@@ -13,5 +18,19 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+        String[] names={"张三","李四","王五"};
+        Observable.from(names).subscribe(new Action1<String>() {
+            @Override
+            public void call(String name) {
+                System.out.println(name);
+            }
+        });
+
+        Observable.just("I","me","your").subscribe(new Action1<String>() {
+            @Override
+            public void call(String s) {
+                System.out.println(s);
+            }
+        });
     }
 }
